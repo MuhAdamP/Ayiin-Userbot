a4e8740d90c4653271eebd3b008b00c461721850
Adam <101717842+MuhAdamP@users.noreply.github.com>
2022-03-17 16:38:11 +0000

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -31,7 +31,7 @@ async def help(event):
         sleep(3)
         await edit_or_reply(
             event,
-            f"**[✧ 𝙰𝚈𝙸𝙸𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ✧](https://github.com/AyiinXd/Ayiin-Userbot)**\n"
+            f"**[✧ 𝙰𝚈𝙸𝙸𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ✧](https://github.com/muhadamp/Ayiin-Userbot)**\n"
             f"**߷ 𝙹𝚄𝙼𝙻𝙰𝙷** `{len(modules)}` **Modules**\n"
             f"**♕︎ 𝙾𝚆𝙽𝙴𝚁:** [{user.first_name}](tg://user?id={user.id})\n\n"
             f"{ICON_HELP}   {string}"
